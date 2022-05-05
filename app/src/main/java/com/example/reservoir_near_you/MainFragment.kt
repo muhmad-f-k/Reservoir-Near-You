f67cc0da0e5a3b66a3f7c0a9a90d917ace3f83b7
Jonathan Skaue <jonathan@skaue.com>
2022-05-05 18:10:45 +0200

    Forbedringer knyttet til navigasjon, action bar og innlogging/utlogging

--- app/src/main/java/com/example/reservoir_near_you/MainFragment.kt
+++ app/src/main/java/com/example/reservoir_near_you/MainFragment.kt
@@ -4,14 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.*
+import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
+import androidx.navigation.navOptions
 import com.example.reservoir_near_you.databinding.FragmentMainBinding
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
@@ -39,6 +39,7 @@ class MainFragment : Fragment() {
             container,
             false
         )
+        setHasOptionsMenu(true)
 
         return binding.root
     }
@@ -74,6 +75,7 @@ class MainFragment : Fragment() {
                     binding.authButton.text = getString(R.string.logout_button_text)
                     binding.authButton.setOnClickListener {
                         AuthUI.getInstance().signOut(requireContext())
+                        activity?.invalidateOptionsMenu()
                     }
                 }
                 else -> {
@@ -94,5 +96,34 @@ class MainFragment : Fragment() {
                 providers
             ).build(), MainFragment.SIGN_IN_RESULT_CODE
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        if (viewModel.authenticationState.value != LoginViewModel.AuthenticationState.AUTHENTICATED) {
+            Log.d("muhmad", "test")
+            menu.findItem(R.id.map).isEnabled = false
+            menu.findItem(R.id.login_logout).isVisible = false
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return (when(item.itemId) {
+            R.id.map -> {
+                var action = MainFragmentDirections.actionMainFragmentToMapsFragment()
+                view?.findNavController()?.navigate(action)
+                true
+            }
+            R.id.login_logout -> {
+                AuthUI.getInstance().signOut(requireContext())
+                activity?.invalidateOptionsMenu()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        })
     }
 }
--- app/src/main/java/com/example/reservoir_near_you/MapsFragment.kt
+++ app/src/main/java/com/example/reservoir_near_you/MapsFragment.kt
@@ -3,10 +3,14 @@ package com.example.reservoir_near_you
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
+import android.util.Log
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import com.example.reservoir_near_you.databinding.FragmentMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.example.reservoir_near_you.databinding.FragmentMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -15,6 +19,9 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment() {
+
+    private val viewModel by viewModels<LoginViewModel>()
+    private lateinit var binding: FragmentMapsBinding
 
     private val callback = OnMapReadyCallback { googleMap ->
 
@@ -28,12 +35,41 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_maps,
+            container,
+            false
+        )
+        setHasOptionsMenu(true)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        menu.findItem(R.id.map).isVisible= false
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return (when(item.itemId) {
+            R.id.login_logout -> {
+                AuthUI.getInstance().signOut(requireContext())
+                var action = MapsFragmentDirections.actionMapsFragmentToMainFragment()
+                view?.findNavController()?.navigate(action)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        })
     }
 }
