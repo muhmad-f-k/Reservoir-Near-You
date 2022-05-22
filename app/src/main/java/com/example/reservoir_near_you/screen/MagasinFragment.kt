d4a2032b3812e125643e65765d9a6926fa1c995a
Muhmad K. Fauzi <muhmad@live.no>
2022-05-22 16:21:06 +0200

    Added Chart

--- app/src/main/java/com/example/reservoir_near_you/screen/MagasinFragment.kt
+++ app/src/main/java/com/example/reservoir_near_you/screen/MagasinFragment.kt
@@ -1,6 +1,8 @@
 package com.example.reservoir_near_you.screen
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
@@ -16,8 +18,13 @@ import com.example.reservoir_near_you.repository.Repository
 import com.example.reservoir_near_you.viewModelFactories.MagasinViewModelFactory
 import com.example.reservoir_near_you.viewModels.MagasinViewModel
 import com.firebase.ui.auth.AuthUI
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.fragment_magasin.view.*
 
 class MagasinFragment : Fragment() {
 
@@ -25,13 +32,22 @@ class MagasinFragment : Fragment() {
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var binding: FragmentMagasinBinding
 
+/*    BarChart Code*/
+    lateinit var barList : ArrayList<BarEntry>
+    lateinit var barDataSet: BarDataSet
+    lateinit var barData: BarData
+
+/*    PieChart Code*/
+    private lateinit var pieList : ArrayList<PieEntry>
+    private lateinit var pieDataSet:PieDataSet
+    private lateinit var pieData:PieData
+
     val args: MagasinFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         fusedLocationProviderClient =
             LocationServices.getFusedLocationProviderClient(requireContext())
         binding = DataBindingUtil.inflate(
@@ -47,10 +63,41 @@ class MagasinFragment : Fragment() {
         val magasinId = args.magasinId
         viewModel = ViewModelProvider(this, viewModelFactory)[MagasinViewModel::class.java]
         viewModel.getMagasin(magasinId)
-        viewModel.allMagasinResponse.observe(viewLifecycleOwner, Observer { response ->
+        viewModel.magasinRespone.observe(viewLifecycleOwner, Observer { response ->
             if (response.isSuccessful){
-                response.body()?.let { binding.textView.text = it.toString() }
+/*                BarChart Code*/
+                Log.d("wow", "det funker ")
+                barList= ArrayList()
+                barList.add(BarEntry(1f,200f))
+                barList.add(BarEntry(2f,100f))
+                barDataSet= BarDataSet(barList, "Vann Magasin")
+                barData= BarData(barDataSet)
+                barDataSet.setColors(ColorTemplate.JOYFUL_COLORS, 250)
+                binding.barChart.bar_chart.data=barData
+                binding.barChart.bar_chart.description.text = "Magasin Data";
+                binding.barChart.bar_chart.description.textSize =13f
+                barDataSet.valueTextColor= Color.BLACK
+                barDataSet.valueTextSize=15f
+                binding.barChart.bar_chart.setDrawValueAboveBar(true)
+
+
+
+/*                PieChart code*/
+                pieList = ArrayList()
+                pieList.add(PieEntry(1f,200f))
+                pieList.add(PieEntry(2f,100f))
+                pieDataSet= PieDataSet(pieList, "TWH Kapasitet")
+                pieData= PieData(pieDataSet)
+                pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS,250)
+                binding.lineChart.line_chart.data=pieData
+                binding.lineChart.line_chart.description.text = "Twh DATA";
+                binding.lineChart.line_chart.description.textSize =13f
+                pieDataSet.valueTextColor= Color.BLACK
+                pieDataSet.valueTextSize=15f
+
+
             }
+
         })
 
         binding.magasinViewModel = viewModel
